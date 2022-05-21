e4228e0efc159bebb93c6bc85449a90aa5a5ca6c
Jaewon <woin2ee@gmail.com>
2022-05-21 12:14:54 +0900

    Initial Commit

---
+++ NormalCalculator/NormalCalculatorApp.swift
@@ -0,0 +1,20 @@
+//
+//  NormalCalculatorApp.swift
+//  NormalCalculator
+//
+//  Created by Jaewon on 2022/05/21.
+//
+
+import SwiftUI
+
+@main
+struct NormalCalculatorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
